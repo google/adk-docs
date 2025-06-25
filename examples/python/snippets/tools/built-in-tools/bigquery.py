b91f013f941a62380018786f07126eea5ca9875c
Shobhit Singh <shobs@google.com>
2025-06-25 08:41:55 +0000

    Update bigquery snippet with working code

--- examples/python/snippets/tools/built-in-tools/bigquery.py
+++ examples/python/snippets/tools/built-in-tools/bigquery.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
+import asyncio
 
 from google.adk.agents import Agent
 from google.adk.runners import Runner
@@ -28,7 +28,7 @@ import google.auth
 AGENT_NAME = "bigquery_agent"
 APP_NAME = "bigquery_app"
 USER_ID = "user1234"
-SESSION_ID = "session_code_exec_async"
+SESSION_ID = "1234"
 GEMINI_MODEL = "gemini-2.0-flash"
 
 # Define a tool configuration to block any write operations
@@ -38,7 +38,6 @@ tool_config = BigQueryToolConfig(write_mode=WriteMode.BLOCKED)
 # credentials
 # https://cloud.google.com/docs/authentication/provide-credentials-adc
 application_default_credentials, _ = google.auth.default()
---------------------------------------------------------------------------------
 credentials_config = BigQueryCredentialsConfig(
     credentials=application_default_credentials
 )
@@ -49,7 +48,7 @@ bigquery_toolset = BigQueryToolset(
 )
 
 # Agent Definition
-root_agent = Agent(
+bigquery_agent = Agent(
     model=GEMINI_MODEL,
     name=AGENT_NAME,
     description=(
@@ -65,10 +64,8 @@ root_agent = Agent(
 
 # Session and Runner
 session_service = InMemorySessionService()
-session = session_service.create_session(
-    app_name=APP_NAME, user_id=USER_ID, session_id=SESSION_ID
-)
-runner = Runner(agent=code_agent, app_name=APP_NAME, session_service=session_service)
+session = asyncio.run(session_service.create_session(app_name=APP_NAME, user_id=USER_ID, session_id=SESSION_ID))
+runner = Runner(agent=bigquery_agent, app_name=APP_NAME, session_service=session_service)
 
 # Agent Interaction
 def call_agent(query):
@@ -78,9 +75,14 @@ def call_agent(query):
     content = types.Content(role='user', parts=[types.Part(text=query)])
     events = runner.run(user_id=USER_ID, session_id=SESSION_ID, new_message=content)
 
+    print("USER:", query)
     for event in events:
         if event.is_final_response():
             final_response = event.content.parts[0].text
-            print("Agent Response: ", final_response)
+            print("AGENT:", final_response)
 
 call_agent("Are there any ml datasets in bigquery-public-data project?")
+call_agent("Tell me more about ml_datasets.")
+call_agent("Which all tables does it have?")
+call_agent("Tell me more about the census_adult_income table.")
+call_agent("How many rows are there per income bracket?")
