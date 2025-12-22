e5644274c6c73a744bf390f041589126472b6708
Didier Durand <durand.didier@gmail.com>
2025-12-22 07:01:38 +0100

    doc: fixing typos in various files

--- examples/python/tutorial/agent_team/adk-tutorial/step_3/agent.py
+++ examples/python/tutorial/agent_team/adk-tutorial/step_3/agent.py
@@ -14,7 +14,7 @@
 
 # @title Import necessary libraries
 from google.adk.agents import Agent
-from google.adk.models.lite_llm import LiteLlm # For multi-model support
+from google.adk.models.lite_llm import LiteLlm # For multimodel support
 from google.adk.sessions import InMemorySessionService
 from google.adk.runners import Runner
 from google.genai import types # For creating message Content/Parts
